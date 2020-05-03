cab00f2d9fe09d3864d68afe16a9885aaa512a3f
Letícia Vigna <leticia.vigna@hotmail.com>
2020-05-03 19:18:28 -0300

    Requisições de evento com código de status e rota GET de eventos funcional

--- server/routes/event.js
+++ server/routes/event.js
@@ -3,12 +3,20 @@ const event = require('express').Router();
 const eventSchema = require('../models/eventSchema');
 const Event = mongoose.model('Event', eventSchema);
 
-// Get all events
+// Get all events - /event
 event.get('/', (req, res) => {
-  console.log("event GET req ");
+  Event.find({}, (err, docs) => {
+    if (err) {
+      res.status(500);
+      res.send("Erro ao adquirir eventos.");
+    } else {
+      res.status(200);
+      res.send(docs);
+    }
+  });
 });
 
-// Create new event
+// Create new event - /event/new
 event.post('/new', (req, res) => {
   let newEvent = new Event({
     title: req.body.title,
@@ -16,9 +24,14 @@ event.post('/new', (req, res) => {
     endDate: req.body.endDate
   });
   newEvent.save(err => {
-    if (err) return handleError(err);
+    if (err) {
+      res.status(500);
+      res.send("Erro ao inserir o novo evento.");
+    } else {
+      res.status(201);
+      res.send(`Novo evento inserido.`);
+    }
   });
-  res.send("Novo evento inserido.");
 });
 
 module.exports = event;
