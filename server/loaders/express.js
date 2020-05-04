add830f866d816e93fd31f1415880ddf6f505e78
Letícia Vigna <leticia.vigna@hotmail.com>
2020-05-04 14:28:31 -0300

    Servidor permite todos os métodos de requisição

--- server/loaders/express.js
+++ server/loaders/express.js
@@ -5,9 +5,10 @@ const load = async() => {
   const app = express();
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  })
+  });
   app.use(express.json());
   app.use(routes);
   return app;
