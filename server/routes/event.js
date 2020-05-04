872187aefb01dba939966263269b3d8580a9f1fa
Letícia Vigna <leticia.vigna@hotmail.com>
2020-05-04 14:29:16 -0300

    Rota /edit atualiza um documento no MongoDB Atlas

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const ObjectId = require('mongoose').Types.ObjectId; 
 const event = require('express').Router();
 const eventSchema = require('../models/eventSchema');
 const Event = mongoose.model('Event', eventSchema);
 
-// Get all events - /event
+// GET: Get all events - /event
 event.get('/', (req, res) => {
   Event.find({}, (err, docs) => {
     if (err) {
@@ -16,12 +17,12 @@ event.get('/', (req, res) => {
   });
 });
 
-// Create new event - /event/new
+// POST: Create new event - /event/new
 event.post('/new', (req, res) => {
   let newEvent = new Event({
     title: req.body.title,
-    start: req.body.startDate,
-    end: req.body.endDate
+    start: req.body.start,
+    end: req.body.end
   });
   newEvent.save(err => {
     if (err) {
@@ -29,7 +30,22 @@ event.post('/new', (req, res) => {
       res.send("Erro ao inserir o novo evento.");
     } else {
       res.status(201);
-      res.send(`Novo evento inserido.`);
+      res.send("Novo evento inserido.");
+    }
+  });
+});
+
+// PUT: Update an event - /event/edit
+event.put('/edit', async(req, res) => {
+  let doc = await Event.findById(new ObjectId(req.body.id));
+  doc.title = req.body.title;
+  doc.save(err => {
+    if (err) {
+      res.status(500);
+      res.send("Erro ao atualizar o evento.");
+    } else {
+      res.status(200);
+      res.send("Evento atualizado.");
     }
   });
 });
